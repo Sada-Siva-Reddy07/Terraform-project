1ed1fd2433e5ba9694f15195e0852c65f472814b
SADA SIVA REDDY <145244419+Sada-Siva-Reddy07@users.noreply.github.com>
2024-09-12 20:04:14 +0530

    Update ec1.tf

--- ec1.tf
+++ ec1.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ecomm" {
-  ami                         = "ami-07761f3ae34c4478d"
-  instance_type               = "t2.micro"
-  key_name                    = "WORDPRESS"
+  ami                         = "ami-0244e31dc33c47ac0"
+  instance_type               = "t3.micro"
+  key_name                    = "stockhol"
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
   subnet_id                   = aws_subnet.public-subnet1.id
   associate_public_ip_address = true
